e52f1a65808c4b0444ec17a036d60e2a5fd883e0
tremaz <150489884+tremaz@users.noreply.github.com>
2024-04-19 20:55:48 -0300

    Add files via upload

---
+++ del6/src/Usuario.java
@@ -0,0 +1,48 @@
+public class Usuario {
+    private String cpf;
+    private String nome;
+    private String email;
+    private String senha;
+    private boolean logado;
+
+    /* public Usuario(String cpf, String nome, String email, String senha)
+    {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    } */
+
+    public void cadastrar(String cpf, String nome, String email, String senha)
+    {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha; 
+
+        System.out.println("Usuario " + nome + " cadastrado!");
+    }
+
+    public void login(String email, String senha)
+    {
+        if(this.email == email && this.senha == senha)
+        {
+            System.out.println("Login realizado!");
+            this.logado = true;
+        }
+        else
+        {
+            System.err.println("Erro no login!");
+        }
+    }
+
+    public void sair()
+    {
+        this.logado = false;
+    }
+
+    public String status()
+    {
+        return this.cpf + "," +  this.nome + "," +  this.email + "," +  this.senha;
+    }
+}
